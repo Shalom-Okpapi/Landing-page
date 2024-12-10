23ba2e0fb93f265c5bd3d1f3650dbe5bfa072be6
Shalom <saintpius15@gmail.com>
2024-12-10 22:37:52 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// Scroll animation for popping up content
+const animatedItems = document.querySelectorAll("[data-animate]");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+animatedItems.forEach((item) => observer.observe(item));
